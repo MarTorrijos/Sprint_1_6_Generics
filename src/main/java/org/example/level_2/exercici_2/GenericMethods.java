d1461da3528b1ee66693701f580612e54756c928
mar <martorrijos@protonmail.com>
2025-05-06 11:51:28 +0200

    apply feedback

--- src/main/java/org/example/level_2/exercici_2/GenericMethods.java
+++ src/main/java/org/example/level_2/exercici_2/GenericMethods.java
@@ -1,15 +1,10 @@
 package org.example.level_2.exercici_2;
 
-import java.util.List;
-
 public class GenericMethods {
 
-    public GenericMethods() {
-    }
-
-    public void print(List<?>... attribute) {
-        for (List<?> list : attribute) {
-            System.out.println(list);
+    public static <T> void genericMethod(T... params) {
+        for (T param : params) {
+            System.out.print(param + ", ");
         }
     }
 
--- src/main/java/org/example/level_2/exercici_2/Main.java
+++ src/main/java/org/example/level_2/exercici_2/Main.java
@@ -1,17 +1,15 @@
 package org.example.level_2.exercici_2;
 
-import java.util.Arrays;
-import java.util.List;
+import static org.example.level_2.exercici_2.GenericMethods.genericMethod;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<?> list1 = Arrays.asList(1, 2, "tres", 4, new Person("Paco", "Paquez", 39));
-        List<?> list2 = Arrays.asList(99.9, 1, "Bon dia");
 
-        System.out.println("LISTA 1: " + System.lineSeparator() + list1);
-        System.out.println("------------------------------------------------------");
-        System.out.println("LISTA 2: " + System.lineSeparator() + list2);
+        genericMethod(1, 2, "tres", 4, new Person("Paco", "Paquez", 39));
+        System.out.println("---------------------------------------");
+        genericMethod(99.9, 1, "Bon dia");
+
     }
 
 }
